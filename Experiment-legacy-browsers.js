3a64ebd207ec2b373bf05821b80e6438f81e4bbb
gzugiz <futungineering@gmail.com>
2025-10-26 10:01:36 +0100

    Update Experiment-legacy-browsers.js

--- Experiment-legacy-browsers.js
+++ Experiment-legacy-browsers.js
@@ -470,7 +470,7 @@ async function experimentInit() {
     fillColor: [0.1294, 0.8667, 0.1294],
     borderColor: null,
     //color: [0.2941, (- 0.6706), (- 0.6706)],
-    color: [-1.0, - 1.0, - 1.0],
+    color: [(-1.0), (- 1.0), (- 1.0)],
     colorSpace: 'rgb',
     pos: [0, (- 0.3)],
     letterHeight: 0.05,
@@ -3160,6 +3160,7 @@ async function quitPsychoJS(message, isCompleted) {
   
   return Scheduler.Event.QUIT;
 }
+
 
 
 
