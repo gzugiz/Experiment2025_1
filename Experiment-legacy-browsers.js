51a3df01a25d4f2c559033421baf648a9457d054
gzugiz <futungineering@gmail.com>
2025-10-26 17:17:46 +0100

    Update Experiment-legacy-browsers.js

--- Experiment-legacy-browsers.js
+++ Experiment-legacy-browsers.js
@@ -1323,7 +1323,7 @@ function trials_4LoopBegin(trials_4LoopScheduler, snapshot) {
     trials_4 = new TrialHandler({
       psychoJS: psychoJS,
       nReps: 2, method: TrialHandler.Method.RANDOM,
-      //extraInfo: expInfo, originPath: undefined,
+      extraInfo: expInfo, originPath: undefined,
       trialList: TrialHandler.importConditions(psychoJS.serverManager, 'Impulses.xlsx', x),
       seed: undefined, name: 'trials_4'
     });
@@ -3162,6 +3162,7 @@ async function quitPsychoJS(message, isCompleted) {
   
   return Scheduler.Event.QUIT;
 }
+
 
 
 
