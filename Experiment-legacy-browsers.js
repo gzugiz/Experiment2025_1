0ece06c31e078a5477fcc64dd0339a19119e60a1
gzugiz <futungineering@gmail.com>
2025-10-27 00:48:18 +0100

    Update Experiment-legacy-browsers.js

--- Experiment-legacy-browsers.js
+++ Experiment-legacy-browsers.js
@@ -1430,11 +1430,11 @@ function trialsLoopEndIteration(scheduler, snapshot) {
       if (snapshot.finished) {
         // Check for and save orphaned data
         if (psychoJS.experiment.isEntryEmpty()) {
-          psychoJS.experiment.nextEntry(snapshot);
+          //psychoJS.experiment.nextEntry(snapshot);
         }
         scheduler.stop();
       } else {
-        psychoJS.experiment.nextEntry(snapshot);
+        //psychoJS.experiment.nextEntry(snapshot);
       }
     return Scheduler.Event.NEXT;
     }
@@ -1494,11 +1494,11 @@ function trials_2LoopEndIteration(scheduler, snapshot) {
       if (snapshot.finished) {
         // Check for and save orphaned data
         if (psychoJS.experiment.isEntryEmpty()) {
-          psychoJS.experiment.nextEntry(snapshot);
+          //psychoJS.experiment.nextEntry(snapshot);
         }
         scheduler.stop();
       } else {
-        psychoJS.experiment.nextEntry(snapshot);
+        //psychoJS.experiment.nextEntry(snapshot);
       }
     return Scheduler.Event.NEXT;
     }
@@ -1558,11 +1558,11 @@ function trials_3LoopEndIteration(scheduler, snapshot) {
       if (snapshot.finished) {
         // Check for and save orphaned data
         if (psychoJS.experiment.isEntryEmpty()) {
-          psychoJS.experiment.nextEntry(snapshot);
+          //psychoJS.experiment.nextEntry(snapshot);
         }
         scheduler.stop();
       } else {
-        psychoJS.experiment.nextEntry(snapshot);
+        //psychoJS.experiment.nextEntry(snapshot);
       }
     return Scheduler.Event.NEXT;
     }
@@ -3159,6 +3159,7 @@ async function quitPsychoJS(message, isCompleted) {
   
   return Scheduler.Event.QUIT;
 }
+
 
 
 
